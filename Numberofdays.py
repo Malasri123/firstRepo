19be6159f1a0f6c8476851be9a3cc75512517894
Malasri123 <60964195+Malasri123@users.noreply.github.com>
2022-07-08 17:18:58 +0530

    added new script to get from number of days

---
+++ Numberofdays.py
@@ -0,0 +1,57 @@
+import json
+import csv
+from datetime import datetime,date,timedelta
+from urllib2 import urlopen
+#start_date ='2022-05-25'
+#end_date ='2022-07-05'
+
+import argparse
+parser = argparse.ArgumentParser()
+#parser.add_argument('--start_date', default='2022-07-05')
+parser.add_argument('--no_of_days', default='2022-07-06')
+parser.add_argument('--repo_url', default='https://github.com/pkar15/watchlist.git')
+
+args = parser.parse_args()
+
+
+no_of_days = args.no_of_days
+start_date = date.today()
+end_date=start_date-timedelta(no_of_days)
+repo_url = args.repo_url
+
+
+def validate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, "%Y-%m-%d")
+    except ValueError:
+        return False
+    return True
+
+if validate(start_date) and validate(end_date) :
+    url= repo_url[:8] + "api." + repo_url[8:]
+    if url.find(".com") != -1:
+        index = url.find(".com")
+        url = url[:index+5] + "repos/" + url[index+5:]
+    if url[-1] == "/":
+        url = url[:-1]
+    if url.endswith(".git"):
+        url = url[:-4]
+    print url
+
+    weather = urlopen(url + '/commits?since={0}T00:00:00Z&until={1}T23:59:59Z'.format(start_date,end_date))
+    wjson = weather.read()
+    wjdata = json.loads(wjson)
+    rows = []
+    tr = []
+    for i in wjdata:
+        tr = [i["sha"],i["commit"]["author"]["name"],i["commit"]["author"]["email"],i["commit"]["author"]["date"],i["commit"]["message"]]
+        rows.append(tr)
+    fields = ['commit id', 'Name', 'Email', 'Date', 'CommitMessage']
+    filename ="gitreport.csv"
+
+    with open (filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(fields)
+        csvwriter.writerows(rows)
+else :
+    print "Please enter correct date format. YY-MM-DD"
